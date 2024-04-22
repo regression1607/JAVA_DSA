6af8607bf39b9178cabdecb8e7a26eb10c8d0c6a
Sanjay Aggarwal <ekansh2020cds102@abesit.edu.in>
2024-04-22 21:48:37 +0530

    first

---
+++ 2.pattern/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle{
+    public static void main (String[] args){
+        System.out.println("This is a rectangle pattern program in Java.");
+        for (int i =1 ; i<=5 ; i++){
+            for (int j =1; j<=5;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
